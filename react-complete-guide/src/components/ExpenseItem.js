fb46b0c7d9053e9b8398ca9aadd6ec0100b41b3a
chemicalstan <chemicalstan15@gmail.com>
2022-12-08 10:49:36 +0100

    Implement composition component

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,3 +1,17 @@
-function ExpenseItem() {
-  return <h1>Expense Item here</h1>;
+import Card from "./Card";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price ">${props.amount}</div>
+      </div>
+    </Card>
+  );
 }
+
+export default ExpenseItem;
