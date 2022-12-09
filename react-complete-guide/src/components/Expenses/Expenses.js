f3c9a74b8d18e1b45da559298f39aabe80b5ae30
chemicalstan <chemicalstan15@gmail.com>
2022-12-09 19:53:10 +0100

    Implement Expense filter

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,15 +1,31 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  // const Expenses = props.items.map((item) => {
+  //   return <ExpenseItem {...item} />;
+  // });
+  // console.log(props.items);
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const newFilterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
-    <Card className="expenses">
-      <ExpenseItem {...props.items[0]} />
-      <ExpenseItem {...props.items[1]} />
-      <ExpenseItem {...props.items[2]} />
-      <ExpenseItem {...props.items[3]} />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onFilterChange={newFilterHandler}
+        />
+        <ExpenseItem {...props.items[0]} />
+        <ExpenseItem {...props.items[1]} />
+        <ExpenseItem {...props.items[2]} />
+        <ExpenseItem {...props.items[3]} />
+      </Card>
+    </div>
   );
 };
 
