428a03ac66d65eb842c40639e56cd45b200f7fcc
chemicalstan <chemicalstan15@gmail.com>
2022-12-11 12:34:00 +0100

    Apply expense item filters

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -5,11 +5,17 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-  // const Expenses = props.items.map((item) => {
-  //   return <ExpenseItem {...item} />;
-  // });
-  // console.log(props.items);
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("");
+  const filteredItems = filteredYear
+    ? props.items.filter(
+        (item) => item.date.getFullYear().toString() === filteredYear
+      )
+    : props.items;
+  const expenseItems = filteredItems.length ? (
+    filteredItems.map((item) => <ExpenseItem key={item.id} {...item} />)
+  ) : (
+    <h3>No expenses found</h3>
+  );
   const newFilterHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
@@ -20,10 +26,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onFilterChange={newFilterHandler}
         />
-        <ExpenseItem {...props.items[0]} />
-        <ExpenseItem {...props.items[1]} />
-        <ExpenseItem {...props.items[2]} />
-        <ExpenseItem {...props.items[3]} />
+        {expenseItems}
       </Card>
     </div>
   );
