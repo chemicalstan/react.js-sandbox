d803f1e48665e9a4ed26d2a9a9d950c6e0a78e98
chemicalstan <chemicalstan15@gmail.com>
2022-12-11 12:32:57 +0100

    Add null expense filter

--- react-complete-guide/src/components/Expenses/ExpensesFilter.js
+++ react-complete-guide/src/components/Expenses/ExpensesFilter.js
@@ -12,6 +12,7 @@ const ExpensesFilter = ({ selected, onFilterChange }) => {
       <div className="expenses-filter__control">
         <label>Filter by year</label>
         <select value={selected} onChange={filterHandler}>
+          <option value="">All</option>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
