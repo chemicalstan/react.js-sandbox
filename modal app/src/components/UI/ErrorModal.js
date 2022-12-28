03e0df5bd3bbba70c8368244785c26d039bc121f
chemicalstan <chemicalstan15@gmail.com>
2022-12-28 15:10:00 +0100

    Implement modal and backdrop portal

--- modal app/src/components/UI/ErrorModal.js
+++ modal app/src/components/UI/ErrorModal.js
@@ -1,20 +1,37 @@
 import Button from "./Button";
 import Card from "./Card";
 import styles from "./ErrorModal.module.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div onClick={props.onAcknowledge} className={styles.backdrop} />;
+};
+
+const Modal = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>{props.message}</div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onAcknowledge}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
 
 const ErrorModal = (props) => {
   return (
     <>
-      <div onClick={props.onAcknowledge} className={styles.backdrop} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>{props.message}</div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onAcknowledge}>Okay</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop {...props} />,
+        document.getElementById("backdrop-wrapper")
+      )}
+      {ReactDOM.createPortal(
+        <Modal {...props} />,
+        document.getElementById("modal-wrapper")
+      )}
     </>
   );
 };
