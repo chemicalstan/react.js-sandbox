127076d1bed2838e31f358575e28f8348dd59632
chemicalstan <chemicalstan15@gmail.com>
2022-12-28 10:56:39 +0100

    Initialize modal app

--- modal app/src/App.js
+++ modal app/src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import AddUser from "./components/User/AddUser";
+import UsersList from "./components/User/UsersList";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  console.log(users);
+
+  const saveUserHandler = (newUser) => {
+    setUsers((prevUsers) => {
+      return [newUser, ...prevUsers];
+    });
+  };
+
   return (
     <div>
-
+      <AddUser onAddUser={saveUserHandler} />
+      <UsersList users={users} />
     </div>
   );
 }
