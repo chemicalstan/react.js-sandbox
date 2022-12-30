9d5c6b28aaa7fdf1278ee8434c71d7b2229d63d2
chemicalstan <chemicalstan15@gmail.com>
2022-12-30 20:39:30 +0100

    Implement useReducer

--- understanding-hooks-project(login-app)/src/components/Login/Login.js
+++ understanding-hooks-project(login-app)/src/components/Login/Login.js
@@ -1,41 +1,78 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+const emailReducer = (prevState, action) => {
+  if (action.USER_EMAIL) {
+    return {
+      isValid: action.USER_EMAIL.includes("@"),
+      value: action.USER_EMAIL,
+    };
+  }
+  return {
+    isValid: prevState.value.includes("@"),
+    value: prevState.value,
+  };
+};
+
+const passwordReducer = (prevState, action) => {
+  if (action.USER_PASSWORD) {
+    return {
+      isValid: action.USER_PASSWORD.trim().length > 6,
+      value: action.USER_PASSWORD,
+    };
+  }
+
+  return {
+    isValid: prevState.value.trim().length > 6,
+    value: prevState.value,
+  };
+};
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState("");
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    isValid: "",
+    value: "",
+  });
+
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    isValid: "",
+    value: "",
+  });
+
+  const { isValid: isValidEmail, value: enteredEmail } = emailState;
+
+  const { isValid: isValidPassword, value: enteredPassword } = passwordState;
+
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    const identifier = setTimeout(() => {
-      setFormIsValid(
-        enteredEmail.includes("@") && enteredPassword.trim().length > 6
-      );
-    }, 500);
-    return () => {
-      clearTimeout(identifier);
-    };
-  }, [enteredEmail, enteredPassword]);
+    console.log("validating form");
+    setFormIsValid(isValidEmail && isValidPassword);
+  }, [isValidEmail, isValidPassword]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    event.preventDefault();
+    // setEnteredEmail(event.target.value);
+    // let val = event.target.value || "";
+    dispatchEmail({ USER_EMAIL: event.target.value });
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+    event.preventDefault();
+
+    // setEnteredPassword(event.target.value);
+
+    dispatchPassword({ USER_PASSWORD: event.target.value });
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes("@"));
+    dispatchEmail({ INPUT_BLUR: "" });
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({ INPUT_BLUR: "" });
   };
 
   const submitHandler = (event) => {
@@ -48,28 +85,26 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ""
+            isValidEmail === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ""
+            isValidPassword === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
